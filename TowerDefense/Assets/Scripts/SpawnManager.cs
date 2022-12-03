f2ba7737ff95b52d6c6edffbe4b031fcd4fabece
loganwade42 <116771844+loganwade42@users.noreply.github.com>
2022-12-02 18:21:44 -0800

    UI
    Added round counter, enemy counter, and delay between rounds with a countdown. right now the countdown is set to 5 seconds, but we can change it to be higher when the game is done.

--- TowerDefense/Assets/Scripts/SpawnManager.cs
+++ TowerDefense/Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class SpawnManager : MonoBehaviour
     public Transform[] spawnLocations;
     public int enemyCount;
     public int level = 1;
+    public int maxLevel = 15;
+    public float timeBetweenRounds = 5.0f;
+    public float currentTime = 0;
+    public float countdownTime = 5.0f;
+
+    public TextMeshProUGUI currentRoundText;
+    public TextMeshProUGUI countdownText;
 
     private void Start()
     {
@@ -16,18 +24,43 @@ public class SpawnManager : MonoBehaviour
     private void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
-        if (enemyCount == 0){
-            level++;
-            spawnEnemyWave(level);
+        if (enemyCount == 0 && level <= maxLevel)
+        {
+            //count up current time
+            currentTime += Time.deltaTime;
+
+            countdownTime -= Time.deltaTime;
+
+            //when current time reaches time between rounds, start next round
+            if (currentTime >= timeBetweenRounds)
+            {
+                currentTime = 0;
+                level++;
+                spawnEnemyWave(level);
+            }
+            
         }
+        else if (enemyCount != 0)
+        {
+            countdownText.text = "";
+            countdownTime = 5.0f;
+        }
+
+        if (enemyCount == 0 && level <= maxLevel)
+        {
+            countdownText.text = "Next round in: " + countdownTime.ToString("0");
+        }
+
+        //update round counter text
+        currentRoundText.text = "ROUND: " + level;
     }
 
     private void spawnEnemyWave(int enemiesToSpawn) {
         for (int i = 0; i < enemiesToSpawn; i++ ){
             // Enemy spawns at randomly spawn locations
-            Instantiate(enemyPrefab, spawnLocations[Random.Range(0,spawnLocations.Length)].transform.position, enemyPrefab.transform.rotation) ;
+            Instantiate(enemyPrefab, spawnLocations[Random.Range(0,spawnLocations.Length)].transform.position, enemyPrefab.transform.rotation);
+            
         }
-        
     }
 
 }
